e1eb93328a410c815d3d56733df964e3074dac71
jdovi <jdovi@drydenengineering.com>
2014-02-05 16:57:48 -0500

    added Waiter model and admin module

--- srnotify/notify/admin.py
+++ srnotify/notify/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from notify.models import Waiter
 
 # Register your models here.
+admin.site.register(Waiter)
--- srnotify/notify/models.py
+++ srnotify/notify/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Definition of the waiter class
+class Waiter(models.Model):
+	customer = models.CharField(max_length=100)
+	vYear = models.IntegerField()
+	vMake = models.CharField(max_length=25)
+	vModel = models.CharField(max_length=25)
+	status = models.CharField(max_length=10)
+	hatnum = models.CharField(max_length=1)
+	
+	#return customer name when calling the class.object.all()
+	def __unicode__(self):
+		return self.customer
