657f4b361cbdfabcf934320a0bc53b35f9c04b1b
jdovi <jdovi@drydenengineering.com>
2014-02-06 10:57:48 -0500

    added windows notes

--- srnotify/notify/views.py
+++ srnotify/notify/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 
-# Create your views here.
+# Function to send / request to the index page
 def index(request):
 	return render_to_response('index.html')
